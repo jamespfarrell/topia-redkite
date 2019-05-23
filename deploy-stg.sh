b2271601e8bb5267262d4a7d42cbf65374d28ddf
Artur Ch <chmarus@gmail.com>
2019-05-23 12:19:48 +0200

    Simple script for deploying to stg

---
+++ deploy-stg.sh
@@ -0,0 +1,8 @@
+gatsby build
+
+rsync -a --progress ./docker-compose.yml deploy@web-staging.topia.us:/home/deploy/web
+rsync -a --progress ./dhparam-2048.pem deploy@web-staging.topia.us:/home/deploy/web
+rsync -a --progress ./nginx.staging.conf deploy@web-staging.topia.us:/home/deploy/web
+rsync -a --progress ./public deploy@web-staging.topia.us:/home/deploy/web
+
+ssh deploy@web-staging.topia.us 'cd /home/deploy/web && sudo docker-compose down && sudo docker-compose up -d'
