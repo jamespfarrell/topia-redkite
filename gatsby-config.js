60f53e8701cf1975b3c00bbcde4df803d37e0f2e
Artur Ch <chmarus@gmail.com>
2019-05-24 12:58:02 +0200

    Enable styled components, add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  siteMetadata: {},
+  siteMetadata: {
+    title: "We accelerate social impact",
+    titleTemplate: "%s · topia",
+    description:
+      "We accelerate social impact.",
+    url: "https://topia.us", // No trailing slash allowed!
+    twitterUsername: "@topiaHQ",
+  },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    `gatsby-plugin-styled-components`
   ]
 }
