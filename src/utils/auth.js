ef5d601680d222c85648285c3800f432d81c7727
Artur Ch <chmarus@gmail.com>
2019-05-31 11:31:54 +0200

    Add NGO onboarding endpoints

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,29 @@
 import axios from 'axios'
-import { userLoginUrl } from './routing'
+import { userLoginUrl, ngoUrl } from './routing'
 const isBrowser = typeof window !== `undefined`
 
 export const handleLogin = async ({ email, password }) => {
   if (!isBrowser) return {}
 
-  const params = {
-    email, password
-  }
+  const params = { email, password }
 
   try {
     const response = await axios.post(userLoginUrl, params)
     return response
   } catch (e) {
     return {}
+  }
+}
+
+export const onboardNGO = async ({ email, name, orgName }) => {
+  if (!isBrowser) return {}
+
+  const params = { email, name, orgName }
+
+  try {
+    const response = await axios.post(ngoUrl, params)
+    return response
+  } catch (e) {
+    return e.response.data
   }
 }
--- src/utils/routing.js
+++ src/utils/routing.js
@@ -2,3 +2,4 @@ const API_URL = process.env.API_URL
 
 export const newsletterUrl = `${API_URL}/newsletter`
 export const userLoginUrl = `${API_URL}/user/login`
+export const ngoUrl = `${API_URL}/ngo`
