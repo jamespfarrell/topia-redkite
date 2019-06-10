d861dca0b0a078e79c1904acf8c9a6528e693acc
Artur Ch <chmarus@gmail.com>
2019-06-10 14:49:54 +0200

    Add user and organization utils

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { setPasswordUrl, userUrl } from './routing'
+import { get, userOrganizations, setPasswordUrl, userUrl } from './routing'
 const isBrowser = typeof window !== `undefined`
 
 
@@ -16,12 +16,15 @@ export const setPassword = async ({ token, password }) => {
   }
 }
 
-export const updateProfile = async ({ token, name, email, avatarFile }) => {
+export const updateProfile = async ({ token, name, email, avatarFile, bannerFile }) => {
   if (!isBrowser) return {}
   try {
     let formData = new FormData()
     if(avatarFile) {
       formData.append('avatarFile', avatarFile)
+    }
+    if(bannerFile) {
+      formData.append('bannerFile', bannerFile)
     }
     formData.append('name', name)
     formData.append('email', email)
@@ -49,4 +52,9 @@ export const getProfile = async ({ token }) => {
   } catch (e) {
     return e.response.data
   }
+}
+
+export const getOrganizations = async ({ token }) => {
+  const result = await get(userOrganizations, token)
+  return result
 }
