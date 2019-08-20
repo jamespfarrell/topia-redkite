442be2948e6bb98e81990a783bc8f41763fe496d
Artur Ch <chmarus@gmail.com>
2019-08-20 14:28:57 +0200

    New bg for website

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ import linkedinSvg from '../icons/linkedin.svg'
 import instagramSvg from '../icons/instagram.svg'
 import topiaSvg from '../icons/topia.svg'
 import earthM from '../assets/earth-m.png'
-import earthL from '../assets/earth-l.png'
+import earthNew from '../assets/earth-new.png'
 import { Link } from 'gatsby'
 
 const LandingPageStyle = createGlobalStyle`
@@ -17,7 +17,7 @@ const LandingPageStyle = createGlobalStyle`
     background: #000;
     color: #fff;
     @media only screen and (min-width: 768px) {
-      background-image: url(${earthL});
+      background-image: url(${earthNew});
       background-repeat: no-repeat;
       background-position: top right;
     }
